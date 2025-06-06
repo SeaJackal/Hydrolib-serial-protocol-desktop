57559c2b4c01a4bdd177034892e33fff633f57f0
Vovdenko Dmitriy <vov-dm-an@yandex-team.ru>
2025-06-07 02:56:41 +0300

    New version

--- include/serial_port_stream.hpp
+++ include/serial_port_stream.hpp
@@ -1,54 +1,45 @@
 #ifndef SERIAL_PORT_SREAM_H_
 #define SERIAL_PORT_SREAM_H_
 
-#include <string>
 #include <cstdint>
+#include <string>
 
 #include <termios.h>
 
-class SerialPortStream
-{
+class SerialPortStream {
 public:
-    struct Config
-    {
-    public:
-        enum class Speed
-        {
-            SPEED_115200
-        };
-        enum class Parity
-        {
-            ODD
-        };
-        enum class StopBits
-        {
-            ONE
-        };
+  struct Config {
+  public:
+    enum class Speed { SPEED_115200 };
+    enum class Parity { ODD };
+    enum class StopBits { ONE };
 
-    public:
-        Speed speed;
-        Parity parity;
-        StopBits stop_bits;
-    };
+  public:
+    Speed speed;
+    Parity parity;
+    StopBits stop_bits;
+  };
 
 public:
-    SerialPortStream(const std::string &file_path,
-                     const Config &config = {Config::Speed::SPEED_115200,
-                                             Config::Parity::ODD,
-                                             Config::StopBits::ONE});
-    ~SerialPortStream();
+  SerialPortStream(const std::string &file_path,
+                   const Config &config = {Config::Speed::SPEED_115200,
+                                           Config::Parity::ODD,
+                                           Config::StopBits::ONE});
+  ~SerialPortStream();
 
 private:
-    int fd_;
-    struct termios tty_;
+  int fd_;
+  struct termios tty_;
 
 public:
-    SerialPortStream &operator<<(uint8_t byte);
-    SerialPortStream &operator>>(uint8_t &byte);
+  SerialPortStream &operator<<(uint8_t byte);
+  SerialPortStream &operator>>(uint8_t &byte);
 
-    uint32_t read_all(uint8_t* buffer);
+  uint32_t ReadAll(uint8_t *buffer);
 
-    bool isValid() const;
+  int GetFileDescriptor();
+
+  bool IsValid() const;
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,135 @@
-#include <iostream>
 #include <chrono>
+#include <iostream>
 #include <thread>
+#include <unistd.h>
 
+#include "hydrolib_common.h"
 #include "serial_port_stream.hpp"
 
-#include "hydrolib_serial_protocol_pack.hpp"
+// extern "C" {
+// #include "hydrolib_ring_queue.h"
+// }
+#include "hydrolib_log_distributor.hpp"
+#include "hydrolib_serial_protocol_master.hpp"
 
-class TxQueue : public hydrolib::serialProtocol::SerialProtocolHandler::TxQueueInterface
-{
+#define RING_BUFFER_CAPACITY 2048
+#define PUBLIC_MEMORY_LENGTH 20
+
+// class Transeiver {
+// public:
+//   Transeiver(const std::string &file_path) : serial_stream_(file_path) {
+//     hydrolib_RingQueue_Init(&ring_queue_, ring_buffer_,
+//     RING_BUFFER_CAPACITY);
+//   }
+
+//   hydrolib_ReturnCode Push(const void *buffer, uint32_t length) {
+//     for (uint32_t i = 0; i < length; i++) {
+//       serial_stream_ << static_cast<const uint8_t *>(buffer)[i];
+//     }
+//     return HYDROLIB_RETURN_OK;
+//   }
+
+//   hydrolib_ReturnCode Read(void *data, unsigned data_length, unsigned shift)
+//   {
+//     return hydrolib_RingQueue_Read(&ring_queue_, data, data_length, shift);
+//   }
+
+//   void Drop(unsigned length) { hydrolib_RingQueue_Drop(&ring_queue_, length);
+//   }
+
+//   void Clear() { hydrolib_RingQueue_Clear(&ring_queue_); }
+
+//   void Process() {
+//     uint8_t rx_buffer[255];
+//     unsigned length = serial_stream_.ReadAll(rx_buffer);
+//     hydrolib_RingQueue_Push(&ring_queue_, rx_buffer, length);
+//   }
+
+// public:
+//   SerialPortStream serial_stream_;
+
+//   uint8_t ring_buffer_[RING_BUFFER_CAPACITY];
+//   hydrolib_RingQueue ring_queue_;
+// };
+
+class TestLogStream {
 public:
-    TxQueue(const std::string &file_path) : serial_stream_(file_path)
-    {
+  hydrolib_ReturnCode Push(const void *data, unsigned length) {
+    for (unsigned i = 0; i < length; i++) {
+      std::cout << (reinterpret_cast<const char *>(data))[i];
     }
-
-    hydrolib_ReturnCode Push(void *buffer, uint32_t length) override
-    {
-        for (uint32_t i = 0; i < length; i++)
-        {
-            serial_stream_ << static_cast<uint8_t *>(buffer)[i];
-        }
-        return HYDROLIB_RETURN_OK;
-    }
-
-public:
-    SerialPortStream serial_stream_;
+    return HYDROLIB_RETURN_OK;
+  }
+  hydrolib_ReturnCode Open() { return HYDROLIB_RETURN_OK; }
+  hydrolib_ReturnCode Close() { return HYDROLIB_RETURN_OK; }
 };
+
+class TestPublicMemory {
+public:
+  TestPublicMemory() {
+    for (int i = 0; i < PUBLIC_MEMORY_LENGTH; i++) {
+      memory[i] = i;
+    }
+  }
+
+public:
+  const uint8_t *Read(unsigned address, unsigned length) {
+    if (address + length > PUBLIC_MEMORY_LENGTH) {
+      return nullptr;
+    }
+    return memory + address;
+  }
+
+public:
+  hydrolib_ReturnCode Write(const void *write_buffer, unsigned address,
+                            unsigned length) {
+    if (address + length > PUBLIC_MEMORY_LENGTH) {
+      return HYDROLIB_RETURN_FAIL;
+    }
+    memcpy(&memory[address], write_buffer, length);
+    return HYDROLIB_RETURN_OK;
+  }
+
+public:
+  uint8_t memory[PUBLIC_MEMORY_LENGTH];
+};
+
+inline TestLogStream log_stream;
+inline hydrolib::logger::LogDistributor distributor("[%s] [%l] %m\n",
+                                                    log_stream);
+inline hydrolib::logger::Logger logger("Serializer", 0, distributor);
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    // if (argc < 2)
-    // {
-    //     cout << "Not enough arguments" << endl;
-    //     return -1;
-    // }
+int main(int argc, char *argv[]) {
+  // if (argc < 2) {
+  //   cout << "Not enough arguments" << endl;
+  //   return -1;
+  // }
 
-    char a;
-    char b;
+  char a;
+  char b;
 
-    uint8_t buffer[255];
+  uint8_t buffer[255];
+  // Transeiver transeiver(argv[1]);
+  SerialPortStream transeiver("/dev/ttyUSB0");
+  TestPublicMemory public_memory;
+  int fd = transeiver.GetFileDescriptor();
+  hydrolib::serial_protocol::Master sp_handler(1, fd, public_memory, logger,
+                                               [&]() {});
 
-    // TxQueue tx_queue(argv[1]);
-    TxQueue tx_queue("/dev/ttyUSB0");
+  while (1) {
+    cin >> a;
+    cout << "Transmitting \"" << a << "\"" << endl;
+    sp_handler.TransmitWrite(2, 0, 1, reinterpret_cast<uint8_t *>(&a));
+    sp_handler.TransmitRead(2, 1, 1, reinterpret_cast<uint8_t *>(&b));
 
-    hydrolib::serialProtocol::SerialProtocolHandler sp_handler(1, tx_queue, nullptr, 0);
+    this_thread::sleep_for(chrono::milliseconds(500));
 
-    while (1)
-    {
-        cin >> a;
-        cout << "Transmitting \"" << a << "\"" << endl;
-        sp_handler.TransmitWrite(2, 0, 1, reinterpret_cast<uint8_t *>(&a));
-        sp_handler.TransmitRead(2, 1, 1, reinterpret_cast<uint8_t *>(&b));
-
-        this_thread::sleep_for(chrono::milliseconds(500));
-
-        uint32_t read_count = tx_queue.serial_stream_.read_all(buffer);
-
-        sp_handler.Receive(buffer, read_count);
-
-        if (!sp_handler.ProcessRx())
-        {
-            cout << "Got nothing" << endl;
-        }
-        else
-        {
-            cout << "Got from memory \"" << b << "\"" << endl;
-        }
+    if (sp_handler.ProcessRx() != HYDROLIB_RETURN_OK) {
+      cout << "Got nothing" << endl;
+    } else {
+      cout << "Got from memory \"" << b << "\"" << endl;
     }
+  }
 }
